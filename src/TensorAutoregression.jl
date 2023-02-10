38c0bd7d0c30f472e77ca4d494ab778b11f5dc92
Quint Wiersma <qnt.wrsm@gmail.com>
2023-02-10 11:02:53 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.31

---
+++ src/TensorAutoregression.jl
@@ -0,0 +1,5 @@
+module TensorAutoregression
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using TensorAutoregression
+using Test
+
+@testset "TensorAutoregression.jl" begin
+    # Write your tests here.
+end
