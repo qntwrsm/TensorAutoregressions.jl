5df3076afce7df27cbb478b52d96fde709cd81a4
Quint Wiersma <qnt.wrsm@gmail.com>
2023-03-08 09:14:15 +0100

    Add als and em solver files

---
+++ src/solvers/als.jl
@@ -0,0 +1,10 @@
+#=
+als.jl
+
+    Provides alternating least squares (ALS) optimization routine for estimating 
+    a tensor autoregressive model with static Kruskal coefficient tensor. 
+
+@author: Quint Wiersma <q.wiersma@vu.nl>
+
+@date: 2023/08/02
+=#
---
+++ src/solvers/em.jl
@@ -0,0 +1,10 @@
+#=
+em.jl
+
+    Provides expectation-maximization (EM) optimization routine for estimating a
+    tensor autoregressive model with dynamic Kruskal coefficient tensor.
+
+@author: Quint Wiersma <q.wiersma@vu.nl>
+
+@date: 2023/07/02
+=#
