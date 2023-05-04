916fd861e0af615369805586425742b88e8934bc
Quint Wiersma <qnt.wrsm@gmail.com>
2023-05-04 11:46:43 +0200

    update plotting.jl description

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,8 +1,10 @@
 #=
 plotting.jl
 
-    Provides a collection of plotting methods for tensor autoregressive models, 
-    such as impulse response functions, forecasts, and data plots.
+    Provides a collection of visualization methods for tensor autoregressive 
+    models, such as impulse response functions, forecasts, data plots, and 
+    Kruskal coefficient tensor and tensor error covariance structure 
+    visualization.
 
 @author: Quint Wiersma <q.wiersma@vu.nl>
 
